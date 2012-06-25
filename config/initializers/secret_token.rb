73ae4d6ba89d8db45d9a854e1427a000370f831f
Jack Christensen <jack@jackchristensen.com>
2012-06-25 18:41:48 -0500

    Initial gem version of Fat Free CRM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmMm::Application.config.secret_token = '8239b1e90e75a85272dcce892bd9778db4b5d2cc19c143120711e5cdc75feb83a5adc459b77194c21086d71842af0047aac5d0d3904f94de27c42a2054f2b264'
